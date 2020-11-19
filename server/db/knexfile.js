ac8093facf6e479da7b50cd3be7b78002454f0db
littleprincey <orlandow1928@gmail.com>
2020-11-20 09:43:45 +1300

    seeds and migrations made

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  
   development: {
     client: 'sqlite3',
     connection: {
@@ -15,6 +16,22 @@ module.exports = {
       filename: ':memory:'
     },
     useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
